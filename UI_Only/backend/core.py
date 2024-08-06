8f57a20c327c7b3da22e544360ffe1e09aee055a
Lidor Eliyahu Shelef <ivanlidor98@gmail.com>
2024-08-06 19:43:06 +0300

    Deleted all of the Azure stuff

--- UI_Only/backend/core.py
+++ UI_Only/backend/core.py
@@ -7,11 +7,6 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import pandas as pd
 import warnings
 from utilities import format_prompt_template
-from dotenv import load_dotenv
-
-from langchain_openai import AzureOpenAIEmbeddings, AzureChatOpenAI
-from langchain_community.vectorstores.pinecone import Pinecone as PineconeLangChain
-from pinecone import Pinecone
 
 from langchain.chains.llm import LLMChain
 from langchain.chains.conversation.base import ConversationChain
@@ -19,35 +14,19 @@ from langchain.chains.conversation.memory import ConversationSummaryMemory
 
 
 warnings.filterwarnings("ignore")
-load_dotenv()
+# load_dotenv()
 
 os.environ['HF_HOME'] = '/UI Only/hf_cache/'
 
-# pc = Pinecone(
-#     api_key=os.environ.get("PINECONE_API_KEY"),
-#     environmemt=os.environ.get("PINECONE_ENVIRONMENT_REGION"),
-# )
-# https://www.pinecone.io/learn/series/langchain/langchain-conversational-memory/
 @st.cache_resource
-def load_model(flag: bool = False):
-    if flag:
-        model_name = "tiiuae/falcon-7b-instruct"
-        dir_path = 'Tiiuae-falcon-7b-instruct'
-        model_name_is = f"peft-training"
-        output_dir = f'{dir_path}/{model_name_is}'
-        model_final_path = f"{output_dir}/final_model/"
-        model = AutoModelForCausalLM.from_pretrained(model_name)
-        model = PeftModel.from_pretrained(model, model_final_path, local_files_only=True)
-    else:    
-        model = AzureChatOpenAI(
-            openai_api_key=os.environ["AZURE_OPENAI_API_KEY"],
-            azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
-            api_version=os.environ["AZURE_API_VERSION"],
-            deployment_name=os.environ["CHAT_NAME"],
-            temperature=0,
-            verbose=True,
-            # client="ChatCompletion",
-        )
+def load_model():
+    model_name = "tiiuae/falcon-7b-instruct"
+    dir_path = 'Tiiuae-falcon-7b-instruct'
+    model_name_is = f"peft-training"
+    output_dir = f'{dir_path}/{model_name_is}'
+    model_final_path = f"{output_dir}/final_model/"
+    model = AutoModelForCausalLM.from_pretrained(model_name)
+    model = PeftModel.from_pretrained(model, model_final_path, local_files_only=True)
     return model
 
 @st.cache_resource
@@ -56,38 +35,10 @@ def load_tokenizer():
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     return tokenizer
 
-def run_llm_azure(
-    user_prompt: str, chat_history: List[Tuple[str, Any]], df: pd.DataFrame = None, prompt_template: str = None
-) -> Any:
-    formated_prompt = format_prompt_template(
-        user_prompt=user_prompt, df=df, template=prompt_template, chat_history=chat_history
-    )
-    # embeddings = AzureOpenAIEmbeddings(
-    #     openai_api_key=os.environ.get("AZURE_OPENAI_API_KEY"),
-    #     azure_endpoint=os.environ.get("AZURE_OPENAI_ENDPOINT"),
-    #     openai_api_version=os.environ["AZURE_API_VERSION"],
-    #     deployment=os.environ["EMBEDDINGS_NAME"],
-    # )
-    # docsearch = PineconeLangChain.from_existing_index(
-    #     index_name=os.environ.get("PINECONE_INDEX_NAME"), embedding=embeddings
-    # )
-    llm = load_model()
-
-    qa = ConversationChain(llm=llm, memory=ConversationSummaryMemory(llm=llm))
-
-    # qa = ConversationalRetrievalChain.from_llm(
-    #     llm=llm,
-    #     # retriever=docsearch.as_retriever(),
-    #     return_source_documents=True,
-    # )
-    # engineered_query = formated_prompt# + "\n\nDataFrmae:\n" + str(df)
-    # return qa({"question": engineered_query, "chat_history": chat_history})
-    return qa(formated_prompt)
-
-
 def run_llm(user_prompt: str, chat_history: List[Tuple[str, Any]], df: pd.DataFrame = None, prompt_template: str = None) -> str:
     model = load_model(flag=True)
-    tokenizer = load_tokenizer()
+    # model = load_model()
+    # tokenizer = load_tokenizer()
 
     formated_prompt = format_prompt_template(
         user_prompt=user_prompt, df=df, template=prompt_template, chat_history=chat_history
